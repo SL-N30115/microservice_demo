38c85e82841d0c05ab1d09db74c9241dcc004eca
Steven <steven.lai1379@gmail.com>
2024-04-25 22:49:59 +0800

    Added user model

---
+++ user/src/database/repository/user-repository.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const { UserModel } = require("./model");
+
+class userRepository {
+  async CreateUser({ username, firstname, lastname, email, password }) {
+    const user = new UserModel({
+      username,
+      firstname,
+      lastname,
+      email,
+      password,
+    });
+
+    const userResult = await user.save();
+
+    return userResult;
+  }
+
+  async FindUserByEmail(email) {
+    const existingUser = await UserModel.findOne({ email: email });
+
+    return existingUser;
+  }
+
+  async FindUserById(id) {
+    const existingUser = await UserModel.findById(id);
+
+    return existingUser;
+  }
+}
+
+module.exports = userRepository;
